1194ba4cc2cafb8408de86095e57f8ff460f4842
MarinaArrieta <arrietamarina12@gmail.com>
2025-09-21 19:01:56 -0300

    Git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -8,4 +8,10 @@ function App() {
   )
 }
 
+const PORT = process.env.PORT || 3000;
+
+App.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+})
+
 export default App
